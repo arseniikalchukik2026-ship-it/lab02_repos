5e0aca0130c855a112181e35ea954b1c3d655908
arseniikalchukik2026-ship-it <arsenii.kalchuk.ik.2026@lpnu.ua>
2026-09-18 16:24:28 +0300

    Add files via upload

---
+++ Project6/Lab02.cpp
@@ -0,0 +1,26 @@
+// Lab_02.cpp
+// < Кальчук Арсеній >
+// Лабораторна робота № 2.
+// Лінійні програми.
+// Варіант 6
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double x; // вхідний параметр
+	double z1; // результат обчислення 1-го виразу
+	//double z2; // результат обчислення 2-го виразу
+	cout << "x = "; cin >> x;
+	z1 = cos(x) + cos(2 * x) + cos(6 * x) + cos(7 * x);
+	z2 = 4 * cos(x / 2.0) * cos((5.0 / 2) * x) * cos(4 * x);
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+
+
+	cin.get();
+	cin.get();
+	return 0;
+}
