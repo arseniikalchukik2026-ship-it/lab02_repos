350d849e35677618bf931cccd6692b87c0a770fe
Arseniy <arsenii.kalchuk.ik.2026@lpnu.ua>
2026-09-18 20:36:33 +0300

    lokalna zmina pered push

--- Lab02.cpp
+++ Lab02.cpp
@@ -12,7 +12,7 @@ int main()
 	double x; // вхідний параметр
 	double z1; // результат обчислення 1-го виразу
 	//double z2; // результат обчислення 2-го виразу
-	cout << "x = "; cin >> x;
+	cout << "Vvedit x = "; cin >> x;
 	z1 = cos(x) + cos(2 * x) + cos(6 * x) + cos(7 * x);
 	z2 = 4 * cos(x / 2.0) * cos((5.0 / 2) * x) * cos(4 * x);
 	cout << endl;
